3a6fc5547b3730fbd37edd96d9e4ee474512c9fa
sylay1 <46131664+heisenshark@users.noreply.github.com>
2022-08-22 00:31:44 +0200

    Added Transaction Add panel
    used material ui, added simple vingette(without vingette effect :P)

---
+++ BudMan/src/app/Transaction.ts
@@ -0,0 +1,8 @@
+export interface Transaction{
+  id?:number
+  amount:number
+  name: string
+  category:string
+  account:string
+  date: Date
+}
--- BudMan/src/app/components/budman-transactions-page/transaction-add/transaction-add.component.ts
+++ BudMan/src/app/components/budman-transactions-page/transaction-add/transaction-add.component.ts
@@ -1,15 +1,43 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms'
+import {
+  MAT_MOMENT_DATE_FORMATS,
+  MomentDateAdapter,
+  MAT_MOMENT_DATE_ADAPTER_OPTIONS,
+} from '@angular/material-moment-adapter';
+import {DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE} from '@angular/material/core';
 
 @Component({
   selector: 'app-transaction-add',
   templateUrl: './transaction-add.component.html',
-  styleUrls: ['./transaction-add.component.scss']
+  styleUrls: ['./transaction-add.component.scss'],
+  providers:[
+    {provide: MAT_DATE_LOCALE, useValue: 'en-GB'},
+    {
+      provide: DateAdapter,
+      useClass: MomentDateAdapter,
+      deps: [MAT_DATE_LOCALE, MAT_MOMENT_DATE_ADAPTER_OPTIONS],
+    },
+    {provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS},
+  ]
 })
 export class TransactionAddComponent implements OnInit {
 
-  constructor() { }
+  constructor(    private _adapter: DateAdapter<any>,
+    @Inject(MAT_DATE_LOCALE) private _locale: string,
+) {
+  this._locale = 'en-GB'
+  _adapter.setLocale(this._locale)
 
+  }
+
+  date = new FormControl(new Date())
   ngOnInit(): void {
+
+  }
+
+  onSubmit(){
+
   }
 
 }
